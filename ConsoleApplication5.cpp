897167fb4b8905aa50c44c83034e4a0656b3c832
Damir <Damir@DESKTOP-ELD43TT>
2024-03-11 00:22:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+int main() {
+	using namespace std;
+	float n1, n2, action;
+	cout << "Enter number 1: ";
+	cin >> n1;
+	cout << "Enter number 2: ";
+	cin >> n2;
+	char math;
+	cout << "Enter math symbol: ";
+	cin >> math;
+	if (math == '+')
+		action = n1 + n2;
+	else if (math == '-')
+		action = n1 - n2;
+	else if (math == '*')
+		action = n1 * n2;
+	else if (math == '/')
+		action = n1 / n2;
+	cout << "Result: " << action;
+	
+}
